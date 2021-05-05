7627ba555bae035202fd1e29016549503b72479a
Tom Gobillard <tom.gobillard6@etu.univ-lorraine.fr>
2021-05-05 23:38:32 +0200

    correctif affichage commandes ouvertes

--- restaurant/src/main/java/fr/ul/miage/restaurant/Impl/CommandeDAOImpl.java
+++ restaurant/src/main/java/fr/ul/miage/restaurant/Impl/CommandeDAOImpl.java
@@ -40,39 +40,44 @@ public class CommandeDAOImpl extends CommandeDAO {
 		try {
 			String sql = "SELECT * FROM COMMANDE WHERE etat = 'EN PREPARATION'";
 
-			PreparedStatement stmt = connect.prepareStatement(sql);
+			PreparedStatement stmt = connect.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
 			ResultSet result = stmt.executeQuery();
 
 			while (result.next()) {
-				System.out.println("1");
 				try {
-					ligneResultat += "Commande n°" + result.getInt(1) + " (" + result.getTime(3) + " - Table n°" + result.getInt(5) + ") : ";
-					
-					String sql2 = "SELECT * FROM COMPOSITION_CMDE WHERE idcommande = ?";
+					ligneResultat += "Commande n°" + result.getInt(1) + " (" + result.getTime(3) + " - Table n°"
+							+ result.getInt(5) + ") : ";
+
+					String sql2 = "SELECT * FROM COMPOSITION_CMDE WHERE idcommande = ? AND etat = 'EN PREPARATION'";
 					PreparedStatement stmt2 = connect.prepareStatement(sql2);
 					stmt2.setLong(1, result.getInt(1));
 					ResultSet result2 = stmt2.executeQuery();
-					
-					while(result2.next()) {
-						String sql3 = "SELECT * FROM PLAT WHERE idplat = ? AND etat = 'EN PREPARATION'";
+
+					while (result2.next()) {
+						String sql3 = "SELECT * FROM PLAT WHERE idplat = ?";
 						PreparedStatement stmt3 = connect.prepareStatement(sql3);
 						stmt3.setLong(1, result2.getInt(2));
 						ResultSet result3 = stmt3.executeQuery();
-						
-						while(result3.next()) {
-							 ligneResultat += result3.getInt(3) + " " + result3.getString(2) + "\n"; 
+
+						while (result3.next()) {
+							ligneResultat += result2.getInt(4) + " " + result3.getString(2);
+						}
+						if (!result2.isLast()) {
+							ligneResultat += ", ";
 						}
 					}
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
+				ligneResultat += "\n";
 			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return ligneResultat;
 	}
-
 
 	@Override
 	public void creerCommande(int idTable) {
@@ -87,23 +92,22 @@ public class CommandeDAOImpl extends CommandeDAO {
 
 		} catch (Exception e) {
 			// TODO: handle exception
-		}	
+		}
 	}
-	
+
 	@Override
 	public int getLastCommande() {
-		int idLastCommande=-1;
-		
+		int idLastCommande = -1;
+
 		try {
 			String sql = "SELECT MAX(idcommande) FROM Commande";
 			PreparedStatement stmt = connect.prepareStatement(sql);
 			ResultSet result = stmt.executeQuery();
-			if(result.next()) {
+			if (result.next()) {
 				idLastCommande = result.getInt(1);
 			}
-		}
-		catch(Exception e){
-			
+		} catch (Exception e) {
+
 		}
 		return idLastCommande;
 	}
@@ -121,6 +125,6 @@ public class CommandeDAOImpl extends CommandeDAO {
 
 		} catch (Exception e) {
 			// TODO: handle exception
-		}			
+		}
 	}
 }
