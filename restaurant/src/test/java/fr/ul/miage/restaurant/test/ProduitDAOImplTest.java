2069107f67b4ad9a03fe348ceddaa832326227be
Maxime Marigliano <maxim.marigliano@gmail.com>
2021-05-29 16:23:46 +0200

    Maxime : debut test Mockito

--- restaurant/src/test/java/fr/ul/miage/restaurant/test/ProduitDAOImplTest.java
+++ restaurant/src/test/java/fr/ul/miage/restaurant/test/ProduitDAOImplTest.java
@@ -3,9 +3,15 @@ package fr.ul.miage.restaurant.test;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import fr.ul.miage.restaurant.Impl.ProduitDAOImpl;
 import fr.ul.miage.restaurant.models.Produit;
@@ -15,15 +21,33 @@ public class ProduitDAOImplTest {
 	
 	private ProduitDAO<Produit> produitDAO;
 	
+	@Mock private ProduitDAO<Produit> mock_produitDAO;
+	@Rule public MockitoRule rule = MockitoJUnit.rule();
+	
 	@Before
 	public void setUp() throws Exception {
 		produitDAO = new ProduitDAOImpl();
 	}
 	
 	@Test
+	public void testGetProduitbyName() {
+		Mockito.when(produitDAO.create(null)).thenReturn(null);
+		//Produit actual = 
+	}
+	
+	@Test
 	public void testListProduits() {
 		ArrayList<Produit> listProduits = produitDAO.listProduit();
-		assertEquals("Champignons", listProduits.get(0).getLibelle());
+		assertNotEquals(0, listProduits.size());
+	}
+	
+	@Test
+	public void testStockProduit() {
+		ArrayList<Produit> listProduits = produitDAO.listProduit();
+
+		for (Produit produit : listProduits) {
+			assertTrue(produit.getQuantite() >=0 );
+		}
 	}
 
 	@Test
@@ -46,4 +70,9 @@ public class ProduitDAOImplTest {
 		produitDAO.update(produit);
 	}
 
+	private Produit createTestProduit() {
+		Produit produit = new Produit();
+		produit.setLibelle("Salade");
+		return produit;
+	}
 }
